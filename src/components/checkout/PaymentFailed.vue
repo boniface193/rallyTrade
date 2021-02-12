ae4f3b1135d621a3fd26fedd134b9a21d1aecc31
abdulazeeza <abdulazeez@brandmobile.net>
2021-02-12 10:35:29 -0800

    fix bug error when a seller enters quantity

--- src/components/checkout/PaymentFailed.vue
+++ src/components/checkout/PaymentFailed.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="text-center py-5 px-5">
         <h3 class="error--text">Payment Failed</h3>
-        <v-btn class="primary">Go back</v-btn>
+        <v-btn class="primary mx-auto my-3">Go back</v-btn>
     </div>
 </template>
 <script>
