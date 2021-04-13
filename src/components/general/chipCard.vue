d5ab4955103f96786c875007be6b0f556410a889
boniface193 <bonifacedennis40@yahoo.com>
2021-04-13 09:08:14 -0700

    adjustment made

--- src/components/general/chipCard.vue
+++ src/components/general/chipCard.vue
@@ -13,33 +13,33 @@
         </template>
       </v-badge>
       <div class="mx-3">
-        <v-row class="pt-4">
+        <v-row class="pt-2">
           <v-col cols="3" class="text-center">
             <div class="d-flex justify-center">
               <v-img width="40.43px" src="@/assets/images/wire.svg"></v-img>
             </div>
-            <div class="text-caption">
-              <div>WIRE</div>
-              <div>15.04.21</div>
+            <div class="text-caption sm-text">
+              <div class="sm-text">WIRE</div>
+              <div class="sm-text">15.04.21</div>
             </div>
           </v-col>
           <v-col cols="1" class="pt-0">
             <v-divider vertical class="new"></v-divider>
           </v-col>
-          <v-col cols="8" class="text-caption px-0">
-            <div class="d-flex justify-space-between mr-3">
+          <v-col md=""  class="text-caption px-0">
+            <div class="d-flex justify-space-between mr-3 sm-text">
               <div>Account</div>
               <div>Amount</div>
               <div>Status</div>
             </div>
-            <div class="d-flex justify-space-between mr-3">
+            <div class="d-flex justify-space-between mr-3 sm-text">
               <div>854715</div>
               <div>10,000,000</div>
-              <div>
-                <v-icon :color="statusColor">{{status}}</v-icon>
+              <div class=" sm-text">
+                <v-icon :color="statusColor">{{ status }}</v-icon>
               </div>
             </div>
-            <div class="text-body-1 mt-5 mb-0 text-left">message goes here</div>
+            <div class="text-body-1 mt-3 mb-0 text-left">message goes here</div>
 
             <!-- <v-row>
               <v-col cols="4"> Account </v-col>
@@ -58,7 +58,12 @@
         </v-row>
       </div>
     </v-card>
-    <v-chip v-if="active" :color="statusColor" small class="rounded mx-16 d-flex justify-center" >
+    <v-chip
+      v-if="active"
+      :color="statusColor"
+      small
+      class="rounded mx-16 d-flex justify-center"
+    >
       Claim Bonus!
     </v-chip>
     <!-- <v-chip :color="colors" class="rounded mx-5 icon_color" style="bottom: 23vh; position: relative">
@@ -107,6 +112,12 @@ html {
 
 .icon_color {
   color: $body-color !important;
+}
+
+@media (max-width: 280px) {
+  .sm-text {
+    font-size: 9px !important;
+  }
 }
 
 @media (min-width: 426px) {
