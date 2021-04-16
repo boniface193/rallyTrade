5115941f3abca6bd62f838d94d6c64553ca3f2ed
boniface193 <bonifacedennis40@yahoo.com>
2021-04-16 08:59:26 -0700

    updated client

--- src/components/detailPages/deposits/bankingDetails.vue
+++ src/components/detailPages/deposits/bankingDetails.vue
@@ -1,14 +1,18 @@
 <template>
-  <div class="container mx-4">
+  <div class="mx-8">
     <div class="text-center text-h5 font-weight-bold my-8">Banking Details</div>
     <v-row>
-      <v-col cols="3" class="text-body-1">Amount</v-col>
+      <v-col cols="3" class="text-body-1 pr-0">Amount</v-col>
       <v-col cols="2"
         ><div class="chip d-flex justify-center">
           <v-icon class="text-center white--text">mdi-currency-ngn</v-icon>
         </div></v-col
       >
-      <v-col cols="7" class="text-h5 text-black ">10,000,000</v-col>
+      <v-col
+        cols="7"
+        class="text-h5 text-black pl-0 mb-1 grey--text darken-4 font-weight-bold"
+        >10,000,000</v-col
+      >
     </v-row>
     <!-- <div class="d-flex">
       <p>Amount </p> <div class="chip d-flex justify-center mx-5">
@@ -60,14 +64,43 @@
     <div>
       <v-row>
         <v-col cols="5">Account Name</v-col>
-        <v-col cols="7">FRNG LIMITED-CLIENT'S ACCOUNT</v-col>
+        <v-col
+          cols="7"
+          class="grey--text darken-4 font-weight-bold text-caption"
+          >FRNG LIMITED-CLIENT'S ACCOUNT</v-col
+        >
       </v-row>
       <v-row>
         <v-col cols="5">Account Number</v-col>
-        <v-col cols="7">198832466</v-col>
+        <v-col
+          cols="7"
+          class="grey--text darken-4 font-weight-bold text-caption"
+          >198832466</v-col
+        >
       </v-row>
     </div>
-    <v-divider></v-divider>
+    <v-divider class="my-2"></v-divider>
+    <div class="mt-5">
+      <v-row>
+        <v-col cols="5">Select your bank</v-col>
+        <v-col cols="7">
+          <v-select :item="items.text" label="Select your bank" dense outlined>
+            <template v-slot:selection="{ item }">
+              <!-- <v-chip v-if="index === 0">
+                <span>{{ item }}</span>
+              </v-chip> -->
+              <v-icon>{{ item.icon }}</v-icon>
+              <!-- <span
+          v-if="index === 1"
+          class="grey--text caption"
+        >
+          (+{{ value.length - 1 }} others)
+        </span> -->
+            </template>
+          </v-select>
+        </v-col>
+      </v-row>
+    </div>
   </div>
 </template>
 
@@ -77,6 +110,13 @@ export default {
   components: {
     GenCard,
   },
+  data: () => ({
+    items: [
+      { text: "foo", icon: "mdi-home" },
+      { text: "bar", icon: "mdi-home" },
+      { text: "OTHER BANK" },
+    ],
+  }),
 };
 </script>
 
