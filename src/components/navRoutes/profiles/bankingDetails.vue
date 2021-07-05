b10a5df81bf52393b0d8f9a17f5268320045030a
unknown <bonifacedennis99@gmail.com>
2021-07-05 09:24:56 +0100

    profile updatd

--- src/components/navRoutes/profiles/bankingDetails.vue
+++ src/components/navRoutes/profiles/bankingDetails.vue
@@ -3,8 +3,10 @@
     <div class="row">
       <div class="col-sm-5 mt-sm-8" v-for="item in chipCard" :key="item.id">
         <Currency
-          :moneySign="item.moneySign"
-          currencyColor="#9EE8FF"
+          :moneySign="
+            item.currency == 'naira' ? 'mdi-currency-ngn' : 'mdi-currency-usd'
+          "
+          :currencyColor="item.currency == 'naira' ? '#9EE8FF' : '#A0FF9E'"
           style="z-index: 1"
         />
         <GenCard
@@ -13,25 +15,27 @@
         >
           <v-container class="mx-4" style="font-family: 'Inter', sans-serif">
             <div class="d-flex py-3">
-              <img src="@/assets/images/bank-logo/gtbank.jpg" width="10%" />
+              <img :src="item.bankLogo" width="10%" />
               <div class="text-sm-h6 text-subtitle-2 secondary--text mx-3">
-                GTBank
+                {{ item.bankName }}
               </div>
             </div>
             <div class="text-md-h6 text-subtitle-2">
-              Mihai Mladin Marius Razvan
+              {{ item.acctName }}
             </div>
-            <div class="text-md-h6 text-subtitle-2 my-3">12345678912</div>
+            <div class="text-md-h6 text-subtitle-2 my-3">
+              {{ item.acctNum }}
+            </div>
             <div
               class="
                 text-sm-h6 text-subtitle-2
-                success--text
+                secondary--text
                 d-flex
                 mr-3
                 justify-end
               "
             >
-              APPROVED
+              PENDING
             </div>
           </v-container>
         </GenCard>
@@ -61,79 +65,81 @@
         </div>
         <v-divider></v-divider>
         <div class="container">
-          <v-layout row wrap class="mt-3 mx-2">
-            <v-flex class="mx-1" xs="6">
-              <v-text-field
-                outlined
-                dense
-                v-model="clientName"
-                label="Cient Name"
-                required
-                type="text"
-                :rules="nameRules"
-              ></v-text-field
-            ></v-flex>
-            <v-flex class="mx-1" xs="6"
-              ><v-select
-                v-model="selectBank"
-                :items="items"
-                item-text="text"
-                label="Select your bank"
-                dense
-                outlined
-                class="text-caption pa-0"
-                :rules="nameRules"
+          <v-form @submit.prevent="submitBankingDetails">
+            <v-layout row wrap class="mt-3 mx-2">
+              <v-flex class="mx-1" xs="6">
+                <v-text-field
+                  outlined
+                  dense
+                  v-model="clientName"
+                  label="Cient Name"
+                  required
+                  type="text"
+                  :rules="nameRules"
+                ></v-text-field
+              ></v-flex>
+              <v-flex class="mx-1" xs="6"
+                ><v-select
+                  v-model="selectBank"
+                  :items="items"
+                  item-text="text"
+                  label="Select your bank"
+                  dense
+                  outlined
+                  class="text-caption pa-0"
+                  :rules="nameRules"
+                >
+                  <template v-slot:selection="{ item }">
+                    <img :src="item.icon" width="20px" />
+                    <span class="ml-1">{{ item.text }}</span>
+                  </template>
+
+                  <template v-slot:item="{ item }">
+                    <img :src="item.icon" width="20px" class="mr-2" />
+                    <span class="text-caption">{{ item.text }}</span>
+                  </template>
+                </v-select></v-flex
               >
-                <template v-slot:selection="{ item }">
-                  <img :src="item.icon" width="20px" />
-                  <span class="ml-1">{{ item.text }}</span>
-                </template>
 
-                <template v-slot:item="{ item }">
-                  <img :src="item.icon" width="20px" class="mr-2" />
-                  <span class="text-caption">{{ item.text }}</span>
-                </template>
-              </v-select></v-flex
-            >
+              <v-flex class="mx-1" xs="6">
+                <v-text-field
+                  v-model="acctNumber"
+                  :counter="10"
+                  outlined
+                  dense
+                  label="10 Digit NUBAN Account Number"
+                  required
+                  type="tel"
+                  :rules="maxRule"
+                ></v-text-field>
+              </v-flex>
 
-            <v-flex class="mx-1" xs="6">
-              <v-text-field
-                v-model="acctNumber"
-                :counter="10"
-                outlined
-                dense
-                label="10 Digit NUBAN Account Number"
-                required
-                type="tel"
-                :rules="maxRule"
-              ></v-text-field>
-            </v-flex>
+              <v-flex class="py-0 col-sm-6">
+                <v-radio-group v-model="currency" row>
+                  <v-radio label="Naira" id="one" value="naira"></v-radio>
+                  <v-radio id="two" label="Dollars" value="dollars"></v-radio>
+                </v-radio-group>
+              </v-flex>
+            </v-layout>
 
-            <v-flex class="py-0 col-sm-6">
-              <v-radio-group v-model="currency" row>
-                <v-radio label="Naira" id="one" value="naira"></v-radio>
-                <v-radio id="two" label="Dollars" value="dollars"></v-radio>
-              </v-radio-group>
-            </v-flex>
-          </v-layout>
-
-          <div>
-            <v-btn
-              type="submit"
-              color="success"
-              depressed
-              class="mt-8"
-              block
-              :disabled="
-                clientName < 1 ||
-                selectBank < 1 ||
-                acctNumber < 1 ||
-                currency == null
-              "
-            >
-              Add
-            </v-btn>
-          </div>
+            <div>
+              <v-btn
+                type="submit"
+                color="success"
+                depressed
+                class="mt-8"
+                block
+                :disabled="
+                  clientName < 1 ||
+                  selectBank < 1 ||
+                  acctNumber < 1 ||
+                  currency == null
+                "
+              >
+                Add
+              </v-btn>
+            </div>
+          </v-form>
         </div>
       </GenCard>
     </Modal>
@@ -145,6 +151,7 @@ import Currency from "@/components/general/currencyBadge.vue";
 import GenCard from "@/components/general/genCard.vue";
 import Modal from "@/components/general/modal.vue";
 import { mapGetters } from "vuex";
+import { v4 as uuidv4 } from "uuid";
 export default {
   components: {
     Currency,
@@ -153,6 +160,7 @@ export default {
   },
   data() {
     return {
+      collect: "",
       clientName: "",
       selectBank: "",
       acctNumber: "",
@@ -175,6 +183,22 @@ export default {
   methods: {
     addBankDetails() {
       this.dialog = true;
+    },
+    submitBankingDetails() {
+      this.$store.commit("profile/setBankingDetails", {
+        id: uuidv4(),
+        msg: "message goes here",
+        currency: this.currency,
+        acctName: this.clientName,
+        acctNum: this.acctNumber,
+        bankLogo: require("@/assets/images/bank-logo/gtbank.jpg"),
+        bankName: this.selectBank,
+      });
+      this.currency = null;
+      this.clientName = "";
+      this.acctNumber = "";
+      this.selectBank = "";
+      this.dialog = false;
     },
   },
 };
--- src/store/modules/profile.js
+++ src/store/modules/profile.js
@@ -1,24 +1,6 @@
 const state = {
     bankDetails: [
-        {
-            id: "deposit002",
-            time: "4:30",
-            msg: "message goes here",
-            moneySign: "mdi-currency-ngn",
-            icon: "wire.svg",
-            depositType: "WIRE",
-            color: "success_bg",
-            status: "mdi-check-circle-outline",
-            statu: "Status",
-            statusText: "APPROVED",
-            statusColor: "success",
-            bonus: true,
-            routes: "",
-            acctName: "",
-            acctNum: "",
-            date: "",
-            active: true,
-        },
+        
     ]
 };
 const getters = {
@@ -31,6 +13,9 @@ const actions = {
 };
 
 const mutations = {
+    setBankingDetails(state, data){
+        state.bankDetails.unshift(data)
+    }
 };
 
 export default {
